3feec51821180b6f2b4bc4749ffe5e2568468503
Edward Potts <pottsed@protonmail.ch>
2021-04-21 14:54:50 +0700

    Initial Commit

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,7 +1,7 @@
 <?php 
 date_default_timezone_set("Asia/Jakarta");
-// $conn = new mysqli("localhost","root","","dtek9578_crm");
-$conn = new mysqli("localhost","dtek9578_main","Protech2021!","dtek9578_crm");
+$conn = new mysqli("localhost","root","","dtek9578_crm");
+// $conn = new mysqli("localhost","dtek9578_main","Protech2021!","dtek9578_crm");
 error_reporting(0);
 session_start();
 ?>
