1cae0f8e4e7405762d8ca35501ac181841d18778
Edward Potts <pottsed@protonmail.ch>
2021-11-30 20:49:06 +0700

    Log Legal

--- module/legal/action.php
+++ module/legal/action.php
@@ -4,13 +4,12 @@ $user   = $_SESSION['id_user'];
 $table  = 'legal';
 $module = $_GET['module'];
 $act    = $_GET['act'];
-function log_data($data){
+function log_data($data = "new entry"){
   global $module;
   global $action;
   global $user;
   global $now;
   global $conn;
-  $data   = isset($data) ? $data : "new entry";
   mysqli_query($conn,"INSERT INTO log (action,module,data,info,created_by,created_at) values ('".ucwords($action)."','".ucwords($module)."','".ucwords($data)."','".ucwords($module." ".$action." ".$data)."','$user','$now')"); 
 }
 $tz = 'Asia/Jakarta';
@@ -18,9 +17,10 @@ $dt = new DateTime("now", new DateTimeZone($tz));
 $now=$dt->format('Y-m-d G:i:s');
 if($act == 'create'){
   $flow         = $_POST['flow'];
+  $project      = $_POST['project'];
   $fileName     = $_FILES['doc']['name'];
   $tmpName      = $_FILES['doc']['tmp_name'];
-  $direktori    = 'documents/';
+  $direktori    = '../../assets/file/';
   $slug         = preg_replace('/[^a-z0-9]+/i', '-', trim(strtolower($fileName)));
   $doc          = $_FILES['doc']['name'];
   $extension    = pathinfo($doc,PATHINFO_EXTENSION);
@@ -33,7 +33,7 @@ if($act == 'create'){
   $docRecepient = $_POST['recipient'];
   $docBody      = '';
   $is_public    = $_POST['is_public'] == true ? 1:0;
-  $is_public    = $_POST['is_sent'] == true ? 1:0;
+  $is_sent    = $_POST['is_sent'] == true ? 1:0;
   $is_delete    = 0;
   $us_approve   = '';
   $approve_date = '';
@@ -43,7 +43,7 @@ if($act == 'create'){
   $updateAt     = $now;
   move_uploaded_file($tmpName,$direktori.$fileName);
 
-  $sql="INSERT INTO legal (id_project,flow,filename,slug,extension,link_doc,doc_number,doc_date,doc_title,doc_author,doc_recepient,doc_body,is_public,is_sent,is_delete,us_approve,approve_date,created_by,created_at,updated_by,updated_at)VALUES(NULL,'$flow','$fileName','$slug','$extension','$link_doc','$docNumb','$docDate','$docTitle','$docAuthor',
+  $sql="INSERT INTO legal (id_project,flow,filename,slug,extension,link_doc,doc_number,doc_date,doc_title,doc_author,doc_recepient,doc_body,is_public,is_sent,is_delete,us_approve,approve_date,created_by,created_at,updated_by,updated_at)VALUES('$project','$flow','$fileName','$slug','$extension','$link_doc','$docNumb','$docDate','$docTitle','$docAuthor',
     '$docRecepient','$docBody','$is_public','$is_sent','$is_delete','$us_approve','$approve_date','$createdBy','$createdAt','$updateBy','$updateAt' );";
 
   $query=mysqli_query($conn,$sql);
@@ -53,7 +53,7 @@ if($act == 'create'){
   header('Location:../../media.php?module='.$module);
 }elseif($act == 'edit') {
     $is_public      = $_POST['is_public'] == true ? 1:0;
-    $is_public      = $_POST['is_sent'] == true ? 1:0;
+    $is_sent      = $_POST['is_sent'] == true ? 1:0;
     $sql= "UPDATE ".$table." SET
     flow            = '".$_POST['flow']."',
     doc_number      = '".$_POST['doc_numb']."',
